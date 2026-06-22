d346fb4a4e4b309b02d3977bd817a48643c8551c
Zarytskyi Oleksandr <pfantom180@gmail.com>
2026-06-22 23:40:32 +0300

    task is done!2

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,70 +7,58 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private static final int INITIAL_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-    private int size = 0;
-    private LinkedList<Entry>[] table;
+
+    private int size;
+    private LinkedList<Entry<K, V>>[] table;
 
     public MyHashMap() {
         table = new LinkedList[INITIAL_CAPACITY];
     }
 
-    public void resize() {
-        LinkedList<Entry>[] oldTable = table;
-        table = new LinkedList[oldTable.length * 2];
-        size = 0;
-
-        for (LinkedList<Entry> bucket : oldTable) {
-            if (bucket != null) {
-                for (Entry entry : bucket) {
-                    put(entry.getKey(), entry.getValue());
-                }
-            }
-        }
-    }
-
     @Override
     public void put(K key, V value) {
-
-        int index = getIndex(key);
+        int bucketIndex = getIndex(key);
         float threshold = table.length * LOAD_FACTOR;
+
         if (size >= threshold) {
             resize();
-            index = getIndex(key);
+            bucketIndex = getIndex(key);
         }
 
-        int myNewIndex = index;
-        if (table[myNewIndex] == null) {
-            LinkedList<Entry> list = new LinkedList<>();
-            list.add(new Entry(key, value));
-            table[myNewIndex] = list;
+        if (table[bucketIndex] == null) {
+            table[bucketIndex] = new LinkedList<>();
+            table[bucketIndex].add(new Entry<>(key, value));
             size++;
-        } else {
-            LinkedList<Entry> listic = table[myNewIndex];
-            for (Entry entry : listic) {
-                if (Objects.equals(entry.getKey(), key)) {
-                    entry.setValue(value);
-                    return;
-                }
+            return;
+        }
+
+        LinkedList<Entry<K, V>> bucket = table[bucketIndex];
+        for (Entry<K, V> entry : bucket) {
+            if (Objects.equals(entry.key, key)) {
+                entry.value = value;
+                return;
             }
-            listic.add(new Entry(key, value));
-            size++;
-
         }
 
+        bucket.add(new Entry<>(key, value));
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        int index = getIndex(key);
-        LinkedList<Entry> list = table[index];
-        if (list == null) {
+        int bucketIndex = getIndex(key);
+        LinkedList<Entry<K, V>> bucket = table[bucketIndex];
+
+        if (bucket == null) {
             return null;
         }
-        for (Entry entry : list) {
-            if (Objects.equals(entry.getKey(), key)) {
-                return entry.getValue();
+
+        for (Entry<K, V> entry : bucket) {
+            if (Objects.equals(entry.key, key)) {
+                return entry.value;
             }
         }
+
         return null;
     }
 
@@ -86,7 +74,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return Math.abs(key.hashCode()) % table.length;
     }
 
-    private class Entry {
+    private void resize() {
+        LinkedList<Entry<K, V>>[] oldTable = table;
+        table = new LinkedList[oldTable.length * 2];
+        size = 0;
+
+        for (LinkedList<Entry<K, V>> bucket : oldTable) {
+            if (bucket != null) {
+                for (Entry<K, V> entry : bucket) {
+                    put(entry.key, entry.value);
+                }
+            }
+        }
+    }
+
+    private class Entry<K, V> {
 
         private K key;
         private V value;
@@ -95,19 +97,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             this.key = key;
             this.value = value;
         }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-
-        public K getKey() {
-            return this.key;
-        }
-
-        public V getValue() {
-            return this.value;
-        }
-
     }
-
 }
